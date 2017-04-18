106f17ce449860ff78dbaec2ae49b4502dd1f7e7
gbt869128710 <869128710@qq.com>
2017-04-18 23:20:58 +0800

    subject
    map

--- 学科发展数据库/js/js.js
+++ 学科发展数据库/js/js.js
@@ -25,15 +25,15 @@ $(function () {
         $(".menus").addClass("h"); //scroll menus hide
         if (w_h >= 200 && w_h <
             1000) {
-            $(".left1,.right1").children().fadeIn(1500);
+            $(".left1,.right1").children().fadeIn(1000);
         } else if (w_h >= 1000 && w_h <
             1800) {
-            $(".service").fadeIn(1500);
+            $(".service").fadeIn(1000);
         } else if (w_h >= 2400 && w_h <
             3000) {
-            $(".subject").fadeIn(1500);
-        } else if (w_h >= 2152) {
-            $(".left,.right").children().fadeIn(1500);
+            $(".subject").fadeIn(1000);
+        } else if (w_h > 2800) {
+            $(".left,.right").children().fadeIn(1000);
         }
         if (w_h >= t_h) {
             $(".anchor").show(); //back to top show
@@ -42,55 +42,70 @@ $(function () {
         }
     }) //move the scroll, content fadeIn
 
+    top();
+
+    function top(val) {
+        $("body").stop(true).animate({
+            "scrollTop": val
+        }, 1000);
+    }
     //click the menu'a ,scroll to position
     $(".menu").children().eq(0).on("click", function () {
 
-        $("body").stop(true).animate({
-            "scrollTop": 0
-        }, 1500);
+        top(0);
 
     })
     $(".menu").children().eq(1).on("click", function () {
+        top(722);
 
-        $("body").stop(true).animate({
-            "scrollTop": 722
-        }, 1500);
 
     })
     $(".menu").children().eq(2).on("click", function () {
 
-        $("body").stop(true).animate({
-            "scrollTop": 900
-        }, 1500);
+        top(900);
 
     })
     $(".menu").children().eq(3).on("click", function () {
 
-        $("body").stop(true).animate({
-            "scrollTop": 1100
-        }, 1500);
+        top(1100);
+
 
     })
     $(".menu").children().eq(4).on("click", function () {
 
-        $("body").stop(true).animate({
-            "scrollTop": 1360
-        }, 1500);
+        top(1360);
+
 
     })
     $(".menu").children().eq(5).on("click", function () {
 
-        $("body").stop(true).animate({
-            "scrollTop": 2582
-        }, 1500);
+        top(2582);
+
 
     })
     $(".menu").children().eq(6).on("click", function () {
 
-        $("body").stop(true).animate({
-            "scrollTop": 3379
-        }, 1500);
+        top(3379);
 
+
+    })
+
+
+    $(".btn").on("click", function () {
+        $(".plat1").fadeIn();
+        // 百度地图API功能
+        var map = new BMap.Map("allmap");
+        var point = new BMap.Point(118.786343, 32.0291430000);
+        map.centerAndZoom(point, 16);
+        var marker = new BMap.Marker(point); // 创建标注
+        map.addOverlay(marker); // 将标注添加到地图中
+        marker.setAnimation(BMAP_ANIMATION_BOUNCE); //跳动的动画
+        map.enableScrollWheelZoom(); //启用滚轮放大缩小，默认禁用
+        map.enableContinuousZoom(); //启用地图惯性拖拽，默认禁用
+
+    })
+    $(".x").on("click", function () {
+        $(".plat1").fadeOut();
     })
     window._bd_share_config = {
         "common": {
@@ -114,5 +129,4 @@ $(function () {
         }
     };
     with(document) 0[(getElementsByTagName('head')[0] || body).appendChild(createElement('script')).src = 'http://bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion=' + ~(-new Date() / 36e5)];
-
 })
